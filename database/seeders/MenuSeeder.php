3e4b614e011c99a3f99012311359413de3b179fa
Drmobkey <yunantobiantoro8@gmail.com>
2025-12-31 14:51:20 +0700

    perbaikan racik menu

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -2,51 +2,35 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\Kitchen;
+use App\Models\Menu;
 
 class MenuSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
         $data = [
-            [
-                'kode' => 'MNDPR0100001',
-                'nama' => 'Nasi Goreng Spesial',
-                'kitchen_id' => 1,
-            ],
-            [
-                'kode' => 'MNDPR0100002',
-                'nama' => 'Mie Ayam Jamur',
-                'kitchen_id' => 1,
-            ],
-            [
-                'kode' => 'MNDPR0200001',
-                'nama' => 'Sate Ayam Madura',
-                'kitchen_id' => 2,
-            ],
-            [
-                'kode' => 'MNDPR0200002',
-                'nama' => 'Gado-Gado Surabaya',
-                'kitchen_id' => 2,
-            ],
-            [
-                'kode' => 'MNDPR0300001',
-                'nama' => 'Rendang Daging Sapi',
-                'kitchen_id' => 3,
-            ],
-            [
-                'kode' => 'MNDPR0300002',
-                'nama' => 'Ayam Bakar Taliwang',
-                'kitchen_id' => 3,
-            ],
+            ['kode' => 'MNDPR0100001', 'nama' => 'Nasi Goreng Spesial', 'kitchen_kode' => 'DPR11'],
+            ['kode' => 'MNDPR0100002', 'nama' => 'Mie Ayam Jamur', 'kitchen_kode' => 'DPR11'],
+            ['kode' => 'MNDPR0200001', 'nama' => 'Sate Ayam Madura', 'kitchen_kode' => 'DPR12'],
+            ['kode' => 'MNDPR0200002', 'nama' => 'Gado-Gado Surabaya', 'kitchen_kode' => 'DPR12'],
+            ['kode' => 'MNDPR0300001', 'nama' => 'Rendang Daging Sapi', 'kitchen_kode' => 'DPR13'],
+            ['kode' => 'MNDPR0300002', 'nama' => 'Ayam Bakar Taliwang', 'kitchen_kode' => 'DPR13'],
         ];
 
-        DB::table('menus')->insert($data);
+        foreach ($data as $item) {
+            $kitchen = Kitchen::where('kode', $item['kitchen_kode'])->first();
+
+            if ($kitchen) {
+                Menu::updateOrCreate(
+                    ['kode' => $item['kode']],
+                    [
+                        'nama' => $item['nama'],
+                        'kitchen_id' => $kitchen->id,
+                    ]
+                );
+            }
+        }
     }
 }
