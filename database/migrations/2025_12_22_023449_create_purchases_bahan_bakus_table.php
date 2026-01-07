1dea939f38d39e3b51b5dcfc38b393589b2ab0cf
hasan1019 <hasanfariki@students.unnes.ac.id>
2026-01-07 09:41:30 +0700

    fix invoice beli bahan baku

--- database/migrations/2025_12_22_023449_create_purchases_bahan_bakus_table.php
+++ database/migrations/2025_12_22_023449_create_purchases_bahan_bakus_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
 
                 $table->decimal('total', 15, 2)->default(0);
                 $table->date('tanggal');
-                $table->date('tanggal')->default(DB::raw('(CURRENT_DATE)'));
                 $table->timestamps();
                 $table->softDeletes();
             });
