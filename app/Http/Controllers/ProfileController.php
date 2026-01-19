d4be3ec442b997fc6e925dd3fc22da05688fa1ac
Drmobkey <yunantobiantoro8@gmail.com>
2026-01-19 14:58:42 +0700

    perubahan profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -17,8 +17,11 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        $user = $request->user();
+
         return view('profile.edit', [
-            'user' => $request->user(),
+            'user' => $user,
+            'kitchens' => $user->kitchens()->orderBy('nama')->get(),
         ]);
     }
 
